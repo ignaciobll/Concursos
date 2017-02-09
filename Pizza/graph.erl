6571cb6789a208cb1207ad9364adcbfc5b9faf2a
Luis <edububa@gmail.com>
2017-02-09 19:07:41 +0100

    get_group, get_groups funs added

--- Pizza/graph.erl
+++ Pizza/graph.erl
@@ -2,7 +2,9 @@
 
 -compile(export_all).
 
--import(lists, [nth/2]).
+-import(lists, [nth/2, member/2, flatten/1, foldl/3, sort/1, seq/2]).
+
+-import(sets, [add_element/2, from_list/1, to_list/1]).
 
 ady(Pos, {N, Gen}) ->
     Len = length(Gen),
@@ -25,19 +27,62 @@ ady(Pos, {N, Gen}) ->
            end}
      }.
 
+explore(Pos, N, Color, Visited, {Color, _, _, _}, Gen, Ady) ->
+    [(Pos - N) |
+     case member(Pos - N, Visited) of
+         true  -> [];
+         false -> get_group(Ady, Pos - N, Visited, Gen)
+     end];
+explore(Pos, _, Color, Visited, {_, Color, _, _}, Gen, Ady) ->
+    [(Pos + 1) |
+     case member(Pos + 1, Visited) of
+         true  -> [];
+         false -> get_group(Ady, Pos + 1, Visited, Gen)
+     end];
+explore(Pos, _, Color, Visited, {_, _, Color, _}, Gen, Ady) ->
+    [(Pos - 1) |
+     case member(Pos - 1, Visited) of
+         true  -> [];
+         false -> get_group(Ady, Pos - 1, Visited, Gen)
+     end];
+explore(Pos, N, Color, Visited, {_, _, _, Color}, Gen, Ady) ->
+    [(Pos + N) |
+     case member(Pos + N, Visited) of
+         true  -> [];
+         false -> get_group(Ady, Pos + N, Visited, Gen)
+     end];
+explore(_, _, _ ,_ , _, _, _) -> [].
+
+get_group(Ady, Pos, Visited, G={N, _Gen}) ->
+    Color = color_of_node(Pos, G),
+    #{ Pos := {Up, Right, Left, Down} } = Ady,
+    Res =
+        [ Pos
+          | [explore(Pos, N, Color, [Pos|Visited], {Up, nop, nop, nop}, G, Ady)
+             | [explore(Pos, N, Color, [Pos|Visited], {nop, Right, nop, nop}, G, Ady)
+                | [explore(Pos, N, Color, [Pos|Visited], {nop, nop, Left, nop}, G, Ady)
+                   | [explore(Pos, N, Color, [Pos|Visited], {nop, nop, nop, Down}, G, Ady)
+                      | []]]]]],
+    clean_list(flatten(Res)).
+
+clean_list(List) -> sort(to_list(from_list(List))).
+
+get_groups(Ady, G={_N, Gen}) ->
+    foldl(fun(X, Acc) -> [get_group(Ady, X, [], G)|Acc] end, [], seq(1, length(Gen))).
+
 %% explore(Pos, {N, Gen}, []) ->
 %%     GPos = nth(Pos, Gen),
-%%     GPosNodes = 
+%%     GPosNodes =
 
-group_of_node(Node, {_N, Gen}) -> lists:nth(Node, Gen).
+color_of_node(Node, {_N, Gen}) -> lists:nth(Node, Gen).
 
-% List of nodes in Gen with group Group
-nodes_of_group(Group, {N, Gen}) ->
-    lists:filter(fun (Pos) -> group_of_node(Pos, {N, Gen}) == Group end, 
-		 lists:seq(1, length(Gen))).
+                                                % List of nodes in Gen with group Group
+nodes_of_color(Color, {N, Gen}) ->
+    lists:filter(fun (Pos) -> color_of_node(Pos, {N, Gen}) == Color end,
+                 lists:seq(1, length(Gen))).
 
 is_ady(Pos, ady, to, Node, {N, Gen}) ->
     #{Pos := TupleAdy } = ady(Pos, {N, Gen}),
     lists:member(Node, tuple_to_list(TupleAdy)).
 
-%% explore(Pos, {N, Gen}, List) ->    
+%% explore(Pos, {N, Gen}, List) ->
